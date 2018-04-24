1f2cc32fd277ec0528cf7f1c79319be3c2074330
van@pi <@>
2018-04-24 17:00:51 +0800

    fix transmissoin

--- tmService.py
+++ tmService.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 def tmService(content,sender):
     print content
     if content=="tm":
-        res=commands.getoutput("transmission-remote -l 2>&1 | sed '$d' | sed 's/[ ][ ]\+/,/g' | awk -F',' 'NR>1{print $2,$10\"\\n\"$9,$7\"kb\\n\"$4,$3}'")
+        res=commands.getoutput("transmission-remote -ne -l 2>&1 | sed '$d' | sed 's/[ ][ ]\+/,/g' | awk -F',' 'NR>1{print $2,$10\"\\n\"$9,$7\"kb\\n\"$4,$3}'")
     else:
         content = content.replace("tm","transmission-remote")
         res=commands.getoutput(content)
