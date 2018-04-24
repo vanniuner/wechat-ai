c770566b487e14c5c9bafd4a2f9a8cc20d9de854
liuzijie <fred.liu@ximalaya.com>
2018-04-24 18:05:57 +0800

    change for python plugin

--- Plugins/music.py
+++ Plugins/music.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 sys.path.append("./project/RasWxNeteaseMusic")
-sys.path.append("./project/RasWxNeteaseMusic/ncmbot")
+sys.path.append("./project/RasWxNeteaseMusic/neteaseApi/ncmbot")
 from WxNeteaseMusic import WxNeteaseMusic
 import ncmbot
 from PluginManager import Model_MenuObj
